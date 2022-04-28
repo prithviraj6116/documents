743ce2cc19bba6e32e659099c55974f683981e1a
Prithviraj Patil <ppatil@ppatil.dhcp.mathworks.com>
2022-04-28 18:06:40 -0400

    abc

--- mwscripts/bashrc.bash
+++ mwscripts/bashrc.bash
@@ -56,7 +56,7 @@ function getTerminalTabTitle() {
     echo $PWD3
 }
   
-alias sbr="sb -r \"openExample('stateflow/AutomaticTransmissionUsingDurationOperatorExample');bdclose all;cd(matlabroot);myStartup; \" &> /tmp/matlab.log &";        
+alias sbr="sb -r \"openExample('stateflow/AutomaticTransmissionUsingDurationOperatorExample');bdclose all;cd(matlabroot);addpath('/mathworks/home/ppatil/Documents/MATLAB/');myStartup; \" &> /tmp/matlab.log &";        
 alias newSession="cd ~;rm -rf logs log matlab_crash_dump* orig.matlab_crash_dump*; pkill -9 -f matlab;sbstop 8 0;pkill -9 -f chrome;pkill -9 -f mozilla; pkill -9 -f firefox; pkill -9 -f p4v;pkill -9 -f gvim;cd -;google-chrome &> /tmp/chrome.log &"
 
 export PS1=..............................................................................\\n$\ 
