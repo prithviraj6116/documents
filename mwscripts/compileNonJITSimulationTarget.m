ea50f8ef066356074e0ee602f4704ee297ec88d8
Prithviraj Patil <prithviraj6116@gmail.com>
2023-01-08 19:07:29 -0500

    abc

--- mwscripts/compileNonJITSimulationTarget.m
+++ mwscripts/compileNonJITSimulationTarget.m
@@ -1,22 +1,37 @@
 function compileNonJITSimulationTarget(modelName)
+    fileGenControlCfg = Simulink.fileGenControl('getConfig');
+    nonJITModelCodeDir = fullfile(fileGenControlCfg.CacheFolder, 'slprj', '_sfprj', modelName);
+    nonJITModelCodeDirContents=dir(nonJITModelCodeDir);
+    for nonJITModelMachineCodeDirIndex = length(nonJITModelCodeDirContents):-1:3
+        nonJITModelMachineCodeDir = nonJITModelCodeDirContents(nonJITModelMachineCodeDirIndex);
+        if nonJITModelMachineCodeDir.isdir
+            if isequal(nonJITModelMachineCodeDir.name,'_self')
+                machineName = modelName;
+            else
+                machineName = nonJITModelMachineCodeDir.name;
+            end
+            compileNonJITSimulationTargetForAMachine(modelName, machineName, nonJITModelMachineCodeDir.name);
+        end
+    end
+end
+function compileNonJITSimulationTargetForAMachine(modelName, machineName, machineFolderName)
     if (isunix)
         archMakeFilExtension = 'mku';
         archDirectory = 'glnxa64';
         archMexExtension = 'mexa64';
         archObjFileExtension = '.o';
         mexCompilerPath = fullfile(matlabroot,'bin', archDirectory, 'gmake');
-        mexCompileCommand = [mexCompilerPath ' -f ' modelName '_sfun.' archMakeFilExtension  ' -j4'];
+        mexCompileCommand = [mexCompilerPath ' -f ' machineName '_sfun.' archMakeFilExtension  ' -j4'];
     elseif (ispc)
         archMexExtension = 'mexw64';
         archObjFileExtension = '.obj';
         mexCompileCommand = ['call ' modelName '_sfun.bat'];
     end
 
-    modelSfMex = [modelName '_sfun.' archMexExtension];
-
-    currDir = pwd;
+    modelSfMex = [modelName '_sfun.' archMexExtension]; 
     fileGenControlCfg = Simulink.fileGenControl('getConfig');
-    nonJITCodeDir = fullfile(fileGenControlCfg.CacheFolder, 'slprj', '_sfprj', modelName, '_self', 'sfun', 'src');
+    nonJITCodeDir = fullfile(fileGenControlCfg.CacheFolder, 'slprj', '_sfprj', modelName, machineFolderName, 'sfun', 'src');
+    currDir = pwd;
 
     if exist(modelSfMex,'file') == 3
          delete(modelSfMex);
@@ -28,7 +43,9 @@ function compileNonJITSimulationTarget(modelName)
     delete(['*' archObjFileExtension]);
 
     system(mexCompileCommand);
-    copyfile(modelSfMex,currDir);
+    if exist(modelSfMex,'file') == 3
+        copyfile(modelSfMex,currDir);
+    end
     cd(currDir);
 end
 
