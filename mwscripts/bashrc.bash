9dfa40b8cdf83fd26ba3ffb9b7bd84339ef6dd57
ppatil <@mathworks.com>
2023-07-07 14:13:24 -0400

    abc

--- mwscripts/bashrc.bash
+++ mwscripts/bashrc.bash
@@ -77,7 +77,7 @@ alias sbmakeq="sbmake -distcc DEBUG=1"
 alias sbmakeq1="sbmake BH_ALLOW_ISOLATED_BUILDS= -distcc DEBUG=1"
 alias sbmakeq2="sbmake NORUNTESTS=1 NOBUILDTESTS=1 BH_ALLOW_ISOLATED_BUILDS= -distcc DEBUG=1"
 alias sbr="sb -r \"addpath('/mathworks/devel/sandbox/ppatil/misc/hubdocs/mwscripts');myStartup;cd('~/Downloads') \" &> /tmp/matlab.log &";        
-alias mynote="cd /mathworks/devel/sandbox/ppatil/misc/hubdocs/notes/;gvim notes.txt & cd - &>/tmp/log1 &"
+alias mynote="cd /mathworks/devel/sandbox/ppatil/misc/hubdocs/notes/;gvim notes.txt & cd - "
 alias newSession="cd ~;rm -rf logs log matlab_crash_dump* orig.matlab_crash_dump*; pkill -9 -f matlab;sbstop 8 0;pkill -9 -f chrome;pkill -9 -f mozilla; pkill -9 -f firefox; pkill -9 -f p4v;pkill -9 -f gvim;cd -;google-chrome &> /tmp/chrome.log & mynote &> /tmp/log1 &"
 #export PS1=..............................................................................\\n$\ 
 export PROMPT_COMMAND='echo -ne "\033]0;$(getTerminalTabTitle)\007"'
